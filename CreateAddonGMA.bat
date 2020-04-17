491e47ecedc1b065293a3ff8ac2ba4087363c03b
Malivil <mlebson@outlook.com>
2020-04-16 22:33:22 -0400

    Updated batch file to work with new file structure

--- CreateAddonGMA.bat
+++ CreateAddonGMA.bat
@@ -1,2 +1,6 @@
 "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmad.exe" create -folder %1
+echo Removing old file
+del TTT-Randomat-20.gma
+echo Renaming file
+ren %1.gma TTT-Randomat-20.gma
 pause
