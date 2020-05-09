e873e14a538e235af74e1c9e798eba9344e212d9
Malivil <mlebson@outlook.com>
2020-05-09 09:00:01 -0400

    Fixed Zombie logic in Taking Inventory event. Primes keep claws, non-primes are excluded entirely. Closes #11

--- Source/lua/randomat2/events/inventory.lua
+++ Source/lua/randomat2/events/inventory.lua
@@ -6,28 +6,43 @@ EVENT.id = "inventory"
 CreateConVar("randomat_inventory_timer", 15, {FCVAR_NOTIFY, FCVAR_ARCHIVE}, "Time between inventory swaps")
 
 function EVENT:Begin()
-    local ply1
-    local ply2
+    local ply1 = nil
+    local ply2 = nil
     timer.Create("RdmtInventoryTimer", GetConVar("randomat_inventory_timer"):GetInt(), 0, function()
         local x = 0
-        for k, v in pairs(self:GetAlivePlayers(true)) do
-            x = x+1
-            if x == 1 then
-                ply1 = v
-            elseif x == 2 then
-                ply2 = v
+        for _, v in pairs(self:GetAlivePlayers(true)) do
+            -- Skip non-prime zombies since they only have claws
+            if v:GetRole() ~= ROLE_ZOMBIE or v:IsZombiePrime() then
+                x = x + 1
+                if x == 1 then
+                    ply1 = v
+                elseif x == 2 then
+                    ply2 = v
+                end
             end
         end
 
+        if ply1 == nil or ply2 == nil then return end
+
         local ply1weps = ply1:GetWeapons()
         ply1:StripWeapons()
-        for k, v in pairs(ply2:GetWeapons()) do
+        -- Have zombies keep their claws
+        if ply1:GetRole() == ROLE_ZOMBIE then
+            ply1:Give("weapon_zom_claws")
+        end
+
+        for _, v in pairs(ply2:GetWeapons()) do
             ply1:Give(v.ClassName)
             ply1:SetFOV(0, 0.2)
         end
 
         ply2:StripWeapons()
-        for k, v in pairs(ply1weps) do
+        -- Have zombies keep their claws
+        if ply2:GetRole() == ROLE_ZOMBIE then
+            ply2:Give("weapon_zom_claws")
+        end
+
+        for _, v in pairs(ply1weps) do
             ply2:Give(v.ClassName)
             ply2:SetFOV(0, 0.2)
         end
