2c5bca29f590a8f0b9f474d4de8a5418ae4f97d3
Malivil <mlebson@outlook.com>
2020-06-20 09:50:43 -0400

    Fixed RISE FROM YOUR GRAVE double-zombying players. Closes #28

--- Source/lua/randomat2/events/grave.lua
+++ Source/lua/randomat2/events/grave.lua
@@ -12,21 +12,25 @@ CreateConVar("randomat_grave_health", 30, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "The he
 function EVENT:Begin()
     timer.Create("infrespawntimer", 1, 0, function()
         for _, ply in pairs(player.GetAll()) do
-            if not ply:Alive() and ply:GetRole() ~= ROLE_ZOMBIE then
+            -- If a player has died and they weren't a zombie or zombifying, zombify them
+            if not ply:Alive() and ply:GetRole() ~= ROLE_ZOMBIE and ply:GetPData("IsZombifying", 0) ~= 1 then
                 if SERVER then
                     net.Start("TTT_Zombified")
 					net.WriteString(ply:Nick())
 					net.Broadcast()
                 end
 
+                local body = ply.server_ragdoll or ply:GetRagdollEntity()
                 ply:SpawnForRound(true)
                 ply:SetCredits(0)
+                ply:SetEyeAngles(Angle(0, body:GetAngles().y, 0))
                 ply:SetRole(ROLE_ZOMBIE)
                 ply:SetZombiePrime(false)
                 ply:SetHealth(GetConVar("randomat_grave_health"):GetInt())
                 ply:SetMaxHealth(GetConVar("randomat_grave_health"):GetInt())
                 ply:StripWeapons()
                 ply:Give("weapon_zom_claws")
+                body:Remove()
                 SendFullStateUpdate()
             end
         end
