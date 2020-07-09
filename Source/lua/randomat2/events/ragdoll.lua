f3fc75311e70cdce8df4425e1e51b2b7b8bb197a
Malivil <mlebson@outlook.com>
2020-07-09 16:31:50 -0400

    Fixed a bunch of bugs with the Bad Trip event. Closes #42

--- Source/lua/randomat2/events/ragdoll.lua
+++ Source/lua/randomat2/events/ragdoll.lua
@@ -3,69 +3,66 @@ local EVENT = {}
 EVENT.Title = "Bad Trip"
 EVENT.id = "ragdoll"
 
-local t = {start=nil,endpos=nil,mask=MASK_PLAYERSOLID,filter=nil}
-local ply = nil
+local ragdolltime = 1.5
+local ragdolldelay = 1.5
 
-local function PlayerNotStuck()
-    t.start = ply:GetPos()
-    t.endpos = t.start
-    t.filter = ply
-
-    return util.TraceEntity(t,ply).StartSolid == false
+local function PlayerNotStuck(ply)
+    local pos = ply:GetPos()
+    local t = {
+        start = pos,
+        endpos = pos,
+        mask = MASK_PLAYERSOLID,
+        filter = ply
+    }
+    return util.TraceEntity(t, ply).StartSolid == false
 end
 
-local function FindPassableSpace( direction, step )
+local function FindPassableSpace(ply, direction, step)
     local i = 0
-    while ( i < 100 ) do
+    while (i < 100) do
         local origin = ply:GetPos()
 
-        --origin = VectorMA( origin, step, direction )
         origin = origin + step * direction
 
-        ply:SetPos( origin )
-        if PlayerNotStuck( ply ) then
-            NewPos = ply:GetPos()
-            return true
+        ply:SetPos(origin)
+        if PlayerNotStuck(ply) then
+            return true, ply:GetPos()
         end
         i = i + 1
     end
-    return false
+    return false, nil
 end
 
 --
 --    Purpose: Unstucks player
 --    Note: Very expensive to call, you have been warned!
 --
-local function UnstuckPlayer( pl )
-    ply = pl
-
-    NewPos = ply:GetPos()
-    local OldPos = NewPos
-
-    if not PlayerNotStuck() then
+local function UnstuckPlayer(ply)
+    if not PlayerNotStuck(ply) then
+        local oldPos = ply:GetPos()
         local angle = ply:GetAngles()
-
         local forward = angle:Forward()
         local right = angle:Right()
         local up = angle:Up()
 
         local SearchScale = 1 -- Increase and it will unstuck you from even harder places but with lost accuracy. Please, don't try higher values than 12
-
-        if  not FindPassableSpace( forward, SearchScale ) and    -- forward
-            not FindPassableSpace( right, SearchScale ) and      -- right
-            not FindPassableSpace( right, -SearchScale ) and     -- left
-            not FindPassableSpace( up, SearchScale ) and        -- up
-            not FindPassableSpace( up, -SearchScale ) and       -- down
-            not FindPassableSpace( forward, -SearchScale )       -- back
-        then
-            --Msg( "Can't find the world for player "..tostring(ply).."\n" )
+        -- Forward
+        local success, pos = FindPassableSpace(ply, forward, SearchScale)
+        if not success then success, pos = FindPassableSpace(ply, right, SearchScale) end -- Right
+        if not success then success, pos = FindPassableSpace(ply, right, -SearchScale) end -- Left
+        if not success then success, pos = FindPassableSpace(ply, up, SearchScale) end -- Up
+        if not success then success, pos = FindPassableSpace(ply, up, -SearchScale) end -- Down
+        if not success then success, pos = FindPassableSpace(ply, forward, -SearchScale) end -- Back
+        if not success then
+            --print("Can't find the location for player "..ply:Nick().."\n")
             return false
         end
 
-        if OldPos == NewPos then
-            return true -- Not stuck?
+        -- Not stuck?
+        if oldPos == pos then
+            return true
         else
-            ply:SetPos( NewPos )
+            ply:SetPos(pos)
             if SERVER and ply and ply:IsValid() and ply:GetPhysicsObject():IsValid() then
                 if ply:IsPlayer() then
                     ply:SetVelocity(vector_origin)
@@ -78,7 +75,7 @@ local function UnstuckPlayer( pl )
     end
 end
 
-local function unragdollPlayer( v )
+local function unragdollPlayer(v)
     v.inRagdoll = false
     v:SetParent()
 
@@ -87,25 +84,26 @@ local function unragdollPlayer( v )
 
     v:Spawn()
 
-    if ragdoll:IsValid() then
+    if ragdoll ~= nil and ragdoll:IsValid() then
         local pos = ragdoll:GetPos()
         pos.z = pos.z + 10
-        v:SetPos( pos )
-        v:SetVelocity( ragdoll:GetVelocity() )
+        v:SetPos(pos)
+        v:SetVelocity(ragdoll:GetVelocity())
         local yaw = ragdoll:GetAngles().yaw
-        v:SetAngles( Angle( 0, yaw, 0 ) )
-        ragdoll:DisallowDeleting( false )
+        v:SetAngles(Angle(0, yaw, 0))
+        ragdoll:DisallowDeleting(false)
         ragdoll:Remove()
     end
 
-    v:SetHealth(v.spawnInfo.health)
     for i, _ in pairs(v.spawnInfo.weps) do
         v:Give(i)
-        local wep = v:GetWeapon( i )
-        if v.spawnInfo.weps[i].Clip then
-            wep:SetClip1(v.spawnInfo.weps[i].Clip)
+        local wep = v:GetWeapon(i)
+        if wep ~= nil and IsValid(wep) then
+            if v.spawnInfo.weps[i].Clip then
+                wep:SetClip1(v.spawnInfo.weps[i].Clip)
+            end
+            v:SetAmmo(v.spawnInfo.weps[i].Reserve, wep:GetPrimaryAmmoType(), true)
         end
-        v:SetAmmo(v.spawnInfo.weps[i].Reserve, wep:GetPrimaryAmmoType(), true)
         v:SelectWeapon(v.spawnInfo.activeWeapon)
     end
 
@@ -117,17 +115,21 @@ local function unragdollPlayer( v )
         end
     end
 
-    timer.Simple(0.1, function()
-        if v:IsInWorld() then
-            UnstuckPlayer(v) -- Thanks to SunRed on GitHub for the unstuck script
-        end
-    end)
-
+    v:SetHealth(math.max(0, v.spawnInfo.health))
+    if v:Health() <= 0 then
+        v:Kill()
+    else
+        timer.Simple(0.1, function()
+            if v:IsInWorld() then
+                UnstuckPlayer(v) -- Thanks to SunRed on GitHub for the unstuck script
+            end
+        end)
+    end
 end
 
-local function ragdollPlayer( v )
+local function ragdollPlayer(v)
     v.inRagdoll = true
-
+    v.lastRagdoll = CurTime()
     v.spawnInfo = {}
 
     local weps = {}
@@ -138,19 +140,11 @@ local function ragdollPlayer( v )
     end
 
     local equipment = {}
-    equipment[EQUIP_RADAR] = false
-    equipment[EQUIP_ARMOR] = false
-    equipment[EQUIP_DISGUISE] = false
-
-    if v:HasEquipmentItem(EQUIP_RADAR) then
-        equipment[EQUIP_RADAR] = true
-    end
-    if v:HasEquipmentItem(EQUIP_ARMOR) then
-        equipment[EQUIP_ARMOR] = true
-    end
-    if v:HasEquipmentItem(EQUIP_DISGUISE) then
-        equipment[EQUIP_DISGUISE] = true
-    end
+    equipment[EQUIP_RADAR] = v:HasEquipmentItem(EQUIP_RADAR)
+    equipment[EQUIP_ARMOR] = v:HasEquipmentItem(EQUIP_ARMOR)
+    equipment[EQUIP_DISGUISE] = v:HasEquipmentItem(EQUIP_DISGUISE)
+    equipment[EQUIP_SPEED] = v:HasEquipmentItem(EQUIP_SPEED)
+    equipment[EQUIP_REGEN] = v:HasEquipmentItem(EQUIP_REGEN)
 
     local info = {}
     info.weps = weps
@@ -160,36 +154,35 @@ local function ragdollPlayer( v )
     info.equipment = equipment
     v.spawnInfo = info
 
-    local ragdoll = ents.Create( "prop_ragdoll" )
+    local ragdoll = ents.Create("prop_ragdoll")
     ragdoll.ragdolledPly = v
-    ragdoll:SetPos( v:GetPos() )
+    ragdoll:SetPos(v:GetPos())
     local velocity = v:GetVelocity()
-    ragdoll:SetAngles( v:GetAngles() )
-    ragdoll:SetModel( v:GetModel() )
+    ragdoll:SetAngles(v:GetAngles())
+    ragdoll:SetModel(v:GetModel())
     ragdoll:Spawn()
     ragdoll:Activate()
-    v:SetParent( ragdoll )
+    v:SetParent(ragdoll)
 
-    for j=0, ragdoll:GetPhysicsObjectCount() -1 do
+    for j=0, ragdoll:GetPhysicsObjectCount() - 1 do
         local phys_obj = ragdoll:GetPhysicsObjectNum(j)
         phys_obj:SetVelocity(velocity)
     end
 
-    v:Spectate( OBS_MODE_CHASE )
-    v:SpectateEntity( ragdoll )
+    v:Spectate(OBS_MODE_CHASE)
+    v:SpectateEntity(ragdoll)
     v:StripWeapons()
 
-    ragdoll:DisallowDeleting( true, function( old, new )
+    ragdoll:DisallowDeleting(true, function(old, new)
         if v:IsValid() then v.ragdoll = new end
     end)
 
     v.ragdoll = ragdoll
     hook.Add("Think", v:Nick().."UnragdollTimer", function()
-        if ragdoll:IsValid() then
-            if ragdoll:GetPhysicsObjectNum( 1 ):GetVelocity():Length() <= 10 then
-                unragdollPlayer(v)
-                hook.Remove("Think", v:Nick().."UnragdollTimer")
-            end
+        -- Turn a ragdoll back into a player if they have essentially stopped moving and have been a ragdoll "long enoughj"
+        if ragdoll:IsValid() and ragdoll:GetPhysicsObjectNum(1):GetVelocity():Length() <= 10 and (CurTime() - v.lastRagdoll) > ragdolltime then
+            hook.Remove("Think", v:Nick().."UnragdollTimer")
+            unragdollPlayer(v)
         end
     end)
 end
@@ -197,11 +190,28 @@ end
 function EVENT:Begin()
     for _, v in pairs(player.GetAll()) do
         v.inRagdoll = false
+        v.lastRagdoll = nil
     end
     hook.Add("Think", "rdmtRagdollInAir", function()
-        for _, v in pairs(self:GetAlivePlayers(true)) do
-            if not v:IsOnGround() and not v.inRagdoll and v:WaterLevel() == 0 then
+        for _, v in pairs(self:GetAlivePlayers()) do
+            -- Turn a player into a ragdoll if they are in the air, not already a ragdoll, not in the water, and haven't been ragdolled recently
+            if not v:IsOnGround() and not v.inRagdoll and v:WaterLevel() == 0 and (v.lastRagdoll == nil or (CurTime() - v.lastRagdoll) > (ragdolltime + ragdolldelay)) then
                 ragdollPlayer(v)
+            end
+        end
+    end)
+
+    hook.Add("EntityTakeDamage", "RdmtRagdollDMGTaken", function(ent, dmg)
+        if not IsValid(ent) or ent:GetClass() ~= "prop_ragdoll" then return end
+        -- Skip crush damage. This is to prevent taking damage just by turning into a ragdoll
+        if dmg:IsDamageType(DMG_CRUSH) then return end
+        for _, v in pairs(player.GetAll()) do
+            for _, j in pairs(ent:GetChildren()) do
+                if j == v then
+                    v.spawnInfo.health = v.spawnInfo.health - dmg:GetDamage()
+                    -- Each ragdoll can only represent a single player so stop both loops early
+                    return
+                end
             end
         end
     end)
@@ -210,22 +220,12 @@ end
 function EVENT:End()
     for _, v in pairs(player.GetAll()) do
         if v.inRagdoll then
-            Unragdoll(v)
-            hook.Remove("Think", v:Nick().."UnragdollTimer")
+            unragdollPlayer(v)
         end
+        hook.Remove("Think", v:Nick().."UnragdollTimer")
     end
     hook.Remove("Think", "rdmtRagdollInAir")
     hook.Remove("EntityTakeDamage", "RdmtRagdollDMGTaken")
 end
-
-hook.Add("EntityTakeDamage", "RdmtRagdollDMGTaken", function(ent, dmg)
-    for _, v in pairs(player.GetAll()) do
-        for _, j in pairs(ent:GetChildren()) do
-            if j == v then
-                v:TakeDamageInfo(dmg)
-            end
-        end
-    end
-end)
 
 Randomat:register(EVENT)
