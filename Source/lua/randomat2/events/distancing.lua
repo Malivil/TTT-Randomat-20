2c36432271a71878fada57fa4b6a2ca6b5b69a3b
Malivil <mlebson@outlook.com>
2020-07-10 08:05:07 -0400

    Fixed Jesters dealing damage in the "Social Distancing" event. Closes #43

--- Source/lua/randomat2/events/distancing.lua
+++ Source/lua/randomat2/events/distancing.lua
@@ -11,26 +11,26 @@ CreateConVar("randomat_distancing_interval", 2, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "
 CreateConVar("randomat_distancing_distance", 100, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Distance a player must be from another to be considered \"near\"", 1, 1000)
 CreateConVar("randomat_distancing_damage", 1, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Damage done to each player who is too close", 1, 100)
 
-local function ClearPlayerData(playername)
-    timer.Remove(playername .. "RdmtDistanceDamageTimer")
-    if playerthinktime[playername] ~= nil then
-        playerthinktime[playername] = nil
+local function ClearPlayerData(uid)
+    timer.Remove(uid .. "RdmtDistanceDamageTimer")
+    if playerthinktime[uid] ~= nil then
+        playerthinktime[uid] = nil
     end
-    if playermoveloc[playername] ~= nil then
-        playermoveloc[playername] = nil
+    if playermoveloc[uid] ~= nil then
+        playermoveloc[uid] = nil
     end
 end
 
 function EVENT:Begin()
     hook.Add("SetupMove", "RdmtDistancingMovementHook", function(ply, mv)
         if ply:Alive() and not ply:IsSpec() then
-            playermoveloc[ply:GetName()] = ply:GetPos()
+            playermoveloc[ply:UniqueID()] = ply:GetPos()
         end
     end);
 
     local plys = {}
     for _, v in pairs(player.GetAll()) do
-        plys[v:GetName()] = v
+        plys[v:UniqueID()] = v
     end
 
     local damagetime = GetConVar("randomat_distancing_timer"):GetInt()
@@ -39,54 +39,54 @@ function EVENT:Begin()
     local damage = GetConVar("randomat_distancing_damage"):GetInt()
     hook.Add("Think", "RdmtDistancingPunishHook", function()
         for _, v in pairs(plys) do
-            local playername = v:GetName()
+            local playeruid = v:UniqueID()
             if v:Alive() and not v:IsSpec() then
                 local playerloc = v:GetPos()
-                local tooclose = false
+                local closeplayer = nil
                 -- Check if this player is within the configurable distance of any other player
-                for name, loc in pairs(playermoveloc) do
-                    if name ~= playername and math.abs(playerloc:Distance(loc)) < distance then
-                        tooclose = true
+                for uid, loc in pairs(playermoveloc) do
+                    if uid ~= playeruid and math.abs(playerloc:Distance(loc)) < distance then
+                        closeplayer = player.GetByUniqueID(uid)
                         break
                     end
                 end
 
                 -- If they are
-                if tooclose then
+                if closeplayer ~= nil then
                     -- and they don't have a time recorded yet then record the time
-                    if playerthinktime[playername] == nil then
-                        playerthinktime[playername] = CurTime()
+                    if playerthinktime[playeruid] == nil then
+                        playerthinktime[playeruid] = CurTime()
                     end
 
                     --- If they have been near other players for more than the configurable amount of time and they aren't already taking damage, start damage
-                    if (CurTime() - playerthinktime[playername]) > damagetime and not timer.Exists(playername .. "RdmtDistanceDamageTimer") then
-                        timer.Create(playername .. "RdmtDistanceDamageTimer", interval, 0, function()
-                            v:TakeDamage(damage, nil, nil)
+                    if (CurTime() - playerthinktime[playeruid]) > damagetime and not timer.Exists(playeruid .. "RdmtDistanceDamageTimer") then
+                        timer.Create(playeruid .. "RdmtDistanceDamageTimer", interval, 0, function()
+                            v:TakeDamage(damage, closeplayer, nil)
                         end)
                     end
 
                 -- Otherwise, stop doing damage
                 else
-                    timer.Remove(playername .. "RdmtDistanceDamageTimer")
-                    if playerthinktime[playername] ~= nil then
-                        playerthinktime[playername] = nil
+                    timer.Remove(playeruid .. "RdmtDistanceDamageTimer")
+                    if playerthinktime[playeruid] ~= nil then
+                        playerthinktime[playeruid] = nil
                     end
                 end
             else
-                if plys[playername] ~= nil then
-                    plys[playername] = nil
+                if plys[playeruid] ~= nil then
+                    plys[playeruid] = nil
                 end
-                ClearPlayerData(playername)
+                ClearPlayerData(playeruid)
             end
         end
     end)
 
     hook.Add("PlayerSpawn", "RdmtDistancingSpawnHook", function(ply)
-        local playername = ply:GetName()
-        if plys[playername] ~= nil then
-            plys[playername] = nil
+        local uid = ply:UniqueID()
+        if plys[uid] ~= nil then
+            plys[uid] = nil
         end
-        ClearPlayerData(playername)
+        ClearPlayerData(uid)
     end)
 end
 
@@ -96,7 +96,7 @@ function EVENT:End()
     hook.Remove("PlayerSpawn", "RdmtDistancingSpawnHook")
 
     for _, v in pairs(player.GetAll()) do
-        ClearPlayerData(v:GetName())
+        ClearPlayerData(v:UniqueID())
     end
 end
 
