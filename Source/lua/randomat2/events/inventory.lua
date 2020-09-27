4d6145213f0a5844639a96e854d179da593db403
Malivil <mlebson@outlook.com>
2020-09-27 09:53:05 -0400

    Fixed Taking Inventory destroying a Hypnotists's brainwashing device. Closes #62

--- Source/lua/randomat2/events/inventory.lua
+++ Source/lua/randomat2/events/inventory.lua
@@ -5,6 +5,31 @@ EVENT.id = "inventory"
 
 CreateConVar("randomat_inventory_timer", 15, {FCVAR_NOTIFY, FCVAR_ARCHIVE}, "Time between inventory swaps")
 
+local function HandleWeapons(ply, weapons, from_killer)
+    local had_brainwash = ply:HasWeapon("weapon_hyp_brainwash")
+    ply:StripWeapons()
+
+    for _, v in pairs(weapons) do
+        ply:Give(WEPS.GetClass(v))
+        ply:SetFOV(0, 0.2)
+    end
+
+    -- Have zombies keep their claws and hypnotists keep their brainwashing device
+    if ply:IsZombie() then
+        ply:Give("weapon_zom_claws")
+    elseif had_brainwash then
+        ply:Give("weapon_hyp_brainwash")
+    end
+
+    -- If the player that swapped to this player was a killer then they no longer have a crowbar
+    if from_killer then
+        ply:Give("weapon_zm_improvised")
+    end
+
+    -- Immediately switch to unarmed to we don't show other players our potentially-illegal first weapon (e.g. Killer's knife)
+    ply:SelectWeapon("weapon_ttt_unarmed")
+end
+
 function EVENT:Begin()
     local ply1 = nil
     local ply2 = nil
@@ -12,7 +37,7 @@ function EVENT:Begin()
         local x = 0
         for _, v in pairs(self:GetAlivePlayers(true)) do
             -- Skip non-prime zombies since they only have claws
-            if v:GetRole() ~= ROLE_ZOMBIE or v:IsZombiePrime() then
+            if not v:IsZombie() or v:IsZombiePrime() then
                 x = x + 1
                 if x == 1 then
                     ply1 = v
@@ -25,37 +50,8 @@ function EVENT:Begin()
         if ply1 == nil or ply2 == nil then return end
 
         local ply1weps = ply1:GetWeapons()
-        ply1:StripWeapons()
-        -- Have zombies keep their claws
-        if ply1:GetRole() == ROLE_ZOMBIE then
-            ply1:Give("weapon_zom_claws")
-        end
-
-        for _, v in pairs(ply2:GetWeapons()) do
-            ply1:Give(v.ClassName)
-            ply1:SetFOV(0, 0.2)
-        end
-
-        -- If the player that swapped to this player was a killer then they no longer have a crowbar
-        if ply2:GetRole() == ROLE_KILLER then
-            ply1:Give("weapon_zm_improvised")
-        end
-
-        ply2:StripWeapons()
-        -- Have zombies keep their claws
-        if ply2:GetRole() == ROLE_ZOMBIE then
-            ply2:Give("weapon_zom_claws")
-        end
-
-        for _, v in pairs(ply1weps) do
-            ply2:Give(v.ClassName)
-            ply2:SetFOV(0, 0.2)
-        end
-
-        -- If the player that swapped to this player was a killer then they no longer have a crowbar
-        if ply1:GetRole() == ROLE_KILLER then
-            ply2:Give("weapon_zm_improvised")
-        end
+        HandleWeapons(ply1, ply2:GetWeapons(), ply2:IsKiller())
+        HandleWeapons(ply2, ply1weps, ply1:IsKiller())
     end)
 end
 
