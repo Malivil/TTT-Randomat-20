a7e75a39ed86af7223440ff03490203bed05138a
Malivil <mlebson@outlook.com>
2020-11-14 18:41:03 -0500

    Fixed Sosig not working for some weapons. Closes #75

--- lua/randomat2/cl_events/cl_sosig.lua
+++ lua/randomat2/cl_events/cl_sosig.lua
@@ -1,5 +1,28 @@
+
+local sosig_sound = "weapons/sosig.mp3"
+
 net.Receive("TriggerSosig", function()
     for _, wep in pairs(LocalPlayer():GetWeapons()) do
-        wep.Primary.Sound = "weapons/sosig.mp3"
+        wep.Primary.OriginalSound = wep.Primary.Sound
+        wep.Primary.Sound = sosig_sound
     end
+
+    hook.Add("EntityEmitSound", "SosigOverrideHook", function(data)
+        local owner = data.Entity
+        if not IsValid(owner) then return end
+        local sound = data.SoundName:lower()
+        local weap_start, _ = string.find(sound, "weapons/")
+        local fire_start, _ = string.find(sound, "fire")
+        if weap_start and fire_start then
+            data.SoundName = sosig_sound
+            return true
+        end
+    end)
+end)
+
+net.Receive("EndSosig", function()
+    for _, wep in pairs(LocalPlayer():GetWeapons()) do
+        wep.Primary.Sound = wep.Primary.OriginalSound
+    end
+    hook.Remove("EntityEmitSound", "SosigOverrideHook")
 end)
--- lua/randomat2/events/sosig.lua
+++ lua/randomat2/events/sosig.lua
@@ -4,26 +4,47 @@ EVENT.Title = "Sosig."
 EVENT.id = "sosig"
 
 util.AddNetworkString("TriggerSosig")
+util.AddNetworkString("EndSosig")
+
+local sosig_sound = "weapons/sosig.mp3"
+
+local function FixWeapon(wep)
+    wep.Primary.OriginalSound = wep.Primary.Sound
+    wep.Primary.Sound = sosig_sound
+end
 
 function EVENT:Begin()
     net.Start("TriggerSosig")
     net.Broadcast()
     for _, ply in pairs(player.GetAll()) do
         for _, wep in pairs(ply:GetWeapons()) do
-            wep.Primary.Sound = "weapons/sosig.mp3"
+            FixWeapon(wep)
         end
     end
     self:AddHook("WeaponEquip", function(wep, ply)
         timer.Create("SosigDelay", 0.1, 1, function()
             net.Start("TriggerSosig")
             net.Send(ply)
-            for _, p in pairs(player.GetAll()) do
-                for _, w in pairs(p:GetWeapons()) do
-                    w.Primary.Sound = "weapons/sosig.mp3"
-                end
-            end
+            FixWeapon(wep)
         end)
     end)
+
+    self:AddHook("EntityEmitSound", function(data)
+        local owner = data.Entity
+        if not IsValid(owner) then return end
+        local sound = data.SoundName:lower()
+        local weap_start, _ = string.find(sound, "weapons/")
+        local fire_start, _ = string.find(sound, "fire")
+        if weap_start and fire_start then
+            data.SoundName = sosig_sound
+            return true
+        end
+    end)
+end
+
+function EVENT:End()
+    net.Start("EndSosig")
+    net.Broadcast()
 end
 
 Randomat:register(EVENT)
