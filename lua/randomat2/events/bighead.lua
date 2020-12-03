babad124a854f2d8469f6f9dc9c7b4fe4c1724dc
Malivil <mlebson@outlook.com>
2020-12-03 07:50:52 -0500

    Fixed head hitboxes in Big Head Mode

--- lua/randomat2/events/bighead.lua
+++ lua/randomat2/events/bighead.lua
@@ -1,24 +1,26 @@
-AddCSLuaFile()
-
 local EVENT = {}
-
-util.AddNetworkString("BigHeadActivate")
-util.AddNetworkString("BigHeadStop")
 
 CreateConVar("randomat_bighead_scale", 2, {FCVAR_NOTIFY, FCVAR_ARCHIVE}, "Head size multiplier", 1.5, 5)
 
 EVENT.Title = "Big Head Mode"
 EVENT.id = "bighead"
 
+local function ScalePlayerHeads(mult)
+    for _, p in pairs(player.GetAll()) do
+        local boneId = p:LookupBone("ValveBiped.Bip01_Head1")
+        if boneId == nil then return end
+
+        local scale = Vector(mult, mult, mult)
+        p:ManipulateBoneScale(boneId, scale)
+    end
+end
+
 function EVENT:Begin()
-    net.Start("BigHeadActivate")
-    net.WriteFloat(GetConVar("randomat_bighead_scale"):GetFloat())
-    net.Broadcast()
+    ScalePlayerHeads(GetConVar("randomat_bighead_scale"):GetFloat())
 end
 
 function EVENT:End()
-    net.Start("BigHeadStop")
-    net.Broadcast()
+    ScalePlayerHeads(1)
 end
 
 function EVENT:GetConVars()
