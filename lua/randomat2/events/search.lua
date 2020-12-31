f0a88caf2d0270c15b93e414cdde56d0bdbec466
Malivil <mlebson@outlook.com>
2020-12-31 08:24:55 -0500

    Changed Dead Men Tell No Tales to show a chat message when attempting to search to explain why it's not working. Closes #105

--- lua/randomat2/events/search.lua
+++ lua/randomat2/events/search.lua
@@ -5,7 +5,10 @@ EVENT.Description = "Prevents corpses from being searched"
 EVENT.id = "search"
 
 function EVENT:Begin()
-    self:AddHook("TTTCanSearchCorpse", function()
+    self:AddHook("TTTCanSearchCorpse", function(ply, rag, covert, long_range, was_traitor)
+        if not ply:IsSpec() and ply:Alive() then
+            ply:ChatPrint("This body refuses to tell you anything -- dead men tell no tales.")
+        end
         return false
     end)
 end
