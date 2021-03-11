db2c6e7323c8768ef87977a69c8c4fce50b5440a
Malivil <mlebson@outlook.com>
2021-03-10 20:48:04 -0500

    Fixed Infinite Ammo for default weapons in Noxx's Custom Roles for TTT

--- lua/randomat2/events/ammo.lua
+++ lua/randomat2/events/ammo.lua
@@ -6,10 +6,12 @@ EVENT.id = "ammo"
 CreateConVar("randomat_ammo_affectbuymenu", 0, {FCVAR_NOTIFY, FCVAR_ARCHIVE}, "Whether it gives buy menu weapons infinite ammo too.")
 
 function EVENT:Begin()
+    local affects_buy = GetConVar("randomat_ammo_affectbuymenu"):GetBool()
     self:AddHook("Think", function()
         for _, v in pairs(self:GetAlivePlayers()) do
-            if not v:GetActiveWeapon().CanBuy or GetConVar("randomat_ammo_affectbuymenu"):GetBool() then
-                v:GetActiveWeapon():SetClip1(v:GetActiveWeapon().Primary.ClipSize)
+            local active_weapon = v:GetActiveWeapon()
+            if IsValid(active_weapon) and (active_weapon.Spawnable or (not active_weapon.CanBuy or affects_buy)) then
+                active_weapon:SetClip1(active_weapon.Primary.ClipSize)
             end
         end
     end)
