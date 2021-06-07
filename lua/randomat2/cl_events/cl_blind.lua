ee7a3f67a512fd32cfd81bc47dff372cfa02cd11
Malivil <mlebson@outlook.com>
2021-06-06 22:08:55 -0400

    Fixed "Blind" event for new Traitor roles

--- lua/randomat2/cl_events/cl_blind.lua
+++ lua/randomat2/cl_events/cl_blind.lua
@@ -2,12 +2,9 @@ net.Receive("blindeventactive", function()
     if net.ReadBool() then
         hook.Add("HUDPaint", "BlindPlayer", function()
             local client = LocalPlayer()
-            if IsValid(client) and client:Alive() then
-                local role = client:GetRole()
-                if role == ROLE_TRAITOR or role == ROLE_ASSASSIN or role == ROLE_HYPNOTIST or role == ROLE_DETRAITOR then
-                    surface.SetDrawColor(0,0,0,255);
-                    surface.DrawRect(0,0,surface.ScreenWidth(),surface.ScreenHeight());
-                end
+            if IsValid(client) and client:Alive() and Randomat:IsTraitorTeam(client) then
+                surface.SetDrawColor(0, 0, 0, 255)
+                surface.DrawRect(0, 0, surface.ScreenWidth(), surface.ScreenHeight())
             end
         end)
     else
