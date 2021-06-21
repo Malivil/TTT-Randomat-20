ca6d8ccf2273aef7423ff066f1f66c6de6c26883
Malivil <mlebson@outlook.com>
2021-06-21 07:37:50 -0400

    Added some sanity checks to prevent client errors in Sosig

--- lua/randomat2/cl_events/cl_sosig.lua
+++ lua/randomat2/cl_events/cl_sosig.lua
@@ -10,35 +10,47 @@ local function FixWeapon(wep)
     wep.Primary.Sound = sosig_sound
 end
 
+local function UnfixWeapon(wep)
+    if not IsValid(wep) or not wep.Primary then return end
+    if wep.Primary.OriginalSound then
+        wep.Primary.Sound = wep.Primary.OriginalSound
+    end
+end
+
 net.Receive("TriggerSosig", function()
-    for _, wep in ipairs(LocalPlayer():GetWeapons()) do
-        FixWeapon(wep)
+    -- This event can be called multiple times but we only want to add the hook once
+    if not hooked then
+        hook.Add("EntityEmitSound", "SosigOverrideHook", function(data)
+            local owner = data.Entity
+            if not IsValid(owner) then return end
+            local sound = data.SoundName:lower()
+            local weap_start, _ = string.find(sound, "weapons/")
+            local fire_start, _ = string.find(sound, "fire")
+            local shot_start, _ = string.find(sound, "shot")
+            if weap_start and (fire_start or shot_start) then
+                data.SoundName = sosig_sound
+                return true
+            end
+        end)
+        hooked = true
     end
 
-    -- This even can be called multiple times but we only want to add the hook once
-    if hooked then return end
+    local client = LocalPlayer()
+    if not IsValid(client) then return end
 
-    hook.Add("EntityEmitSound", "SosigOverrideHook", function(data)
-        local owner = data.Entity
-        if not IsValid(owner) then return end
-        local sound = data.SoundName:lower()
-        local weap_start, _ = string.find(sound, "weapons/")
-        local fire_start, _ = string.find(sound, "fire")
-        local shot_start, _ = string.find(sound, "shot")
-        if weap_start and (fire_start or shot_start) then
-            data.SoundName = sosig_sound
-            return true
-        end
-    end)
-    hooked = true
+    for _, wep in ipairs(client:GetWeapons()) do
+        FixWeapon(wep)
+    end
 end)
 
 net.Receive("EndSosig", function()
-    for _, wep in ipairs(LocalPlayer():GetWeapons()) do
-        if wep.Primary and wep.Primary.OriginalSound then
-            wep.Primary.Sound = wep.Primary.OriginalSound
-        end
-    end
     hook.Remove("EntityEmitSound", "SosigOverrideHook")
     hooked = false
+
+    local client = LocalPlayer()
+    if not IsValid(client) then return end
+
+    for _, wep in ipairs(client:GetWeapons()) do
+        UnfixWeapon(wep)
+    end
 end)
