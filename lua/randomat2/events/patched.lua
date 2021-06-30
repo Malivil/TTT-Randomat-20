3c6b5820d98c735905d4158a3bd93a3cca7860f9
Malivil <mlebson@outlook.com>
2021-06-30 16:48:22 -0400

    Changed "A Glitch has been patched" to a secret event. Closes #170

--- lua/randomat2/events/patched.lua
+++ lua/randomat2/events/patched.lua
@@ -6,6 +6,7 @@ EVENT.Title = "A Glitch has been patched"
 EVENT.Description = "Changes a random Glitch into either an Innocent or a Traitor"
 EVENT.id = "patched"
 EVENT.AltTitle = "Patched"
+EVENT.StartSecret = true
 
 function EVENT:Begin()
     for _, v in ipairs(self:GetAlivePlayers(true)) do
