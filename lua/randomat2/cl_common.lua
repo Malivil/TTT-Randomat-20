d54c460cc8243b1845e51f5742130ea755825aac
Malivil <mlebson@outlook.com>
2021-07-21 19:59:46 -0400

    Added common functions for client UI

--- lua/randomat2/cl_common.lua
+++ lua/randomat2/cl_common.lua
@@ -1,3 +1,4 @@
+-- Player Effects
 function Randomat:HandlePlayerSmoke(client, pred)
     for _, v in ipairs(player.GetAll()) do
         if pred(v) then
@@ -28,5 +29,39 @@ function Randomat:HandlePlayerSmoke(client, pred)
                 v.RdmtSmokeEmitter = nil
             end
         end
+    end
+end
+
+-- UI Functions
+local Tex_Corner8 = surface.GetTextureID("gui/corner8")
+function Randomat:RoundedMeter(bs, x, y, w, h, color)
+    surface.SetDrawColor(color:Unpack())
+
+    surface.DrawRect(x + bs, y, w - bs * 2, h)
+    surface.DrawRect(x, y + bs, bs, h - bs * 2)
+
+    surface.SetTexture(Tex_Corner8)
+    surface.DrawTexturedRectRotated(x + bs / 2, y + bs / 2, bs, bs, 0)
+    surface.DrawTexturedRectRotated(x + bs / 2, y + h - bs / 2, bs, bs, 90)
+
+    if w > 14 then
+        surface.DrawRect(x + w - bs, y + bs, bs, h - bs * 2)
+        surface.DrawTexturedRectRotated(x + w - bs / 2, y + bs / 2, bs, bs, 270)
+        surface.DrawTexturedRectRotated(x + w - bs / 2, y + h - bs / 2, bs, bs, 180)
+    else
+        surface.DrawRect(x + math.max(w - bs, bs), y, bs / 2, h)
+    end
+end
+
+function Randomat:PaintBar(r, x, y, w, h, colors, value)
+    -- Background
+    -- slightly enlarged to make a subtle border
+    draw.RoundedBox(8, x - 1, y - 1, w + 2, h + 2, colors.background)
+
+    -- Fill
+    local width = w * math.Clamp(value, 0, 1)
+
+    if width > 0 then
+        Randomat:RoundedMeter(r, x, y, width, h, colors.fill)
     end
 end
