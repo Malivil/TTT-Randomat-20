c2e9d0cda1f60466b0314a58e27f84abe10862e3
Malivil <mlebson@outlook.com>
2021-09-01 12:20:53 -0400

    Changed Secret event description to be shorter

--- lua/randomat2/events/secret.lua
+++ lua/randomat2/events/secret.lua
@@ -1,7 +1,7 @@
 local EVENT = {}
 
 EVENT.Title = "Shh... It's a Secret!"
-EVENT.Description = "Silences all Randomat event notifications while this event is active and triggers another random event"
+EVENT.Description = "Silences all event notifications and triggers another random event"
 EVENT.id = "secret"
 
 function EVENT:Begin(target_event)
