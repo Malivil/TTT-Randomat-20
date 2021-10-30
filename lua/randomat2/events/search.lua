c21603d065b66ef00fe7b0c4a47e71b0ee1c8278
Malivil <mlebson@outlook.com>
2021-10-30 12:19:09 -0400

    Fixed credits not being lootable when Dead Men Tell No Tales is running

--- lua/randomat2/events/search.lua
+++ lua/randomat2/events/search.lua
@@ -6,6 +6,12 @@ EVENT.id = "search"
 
 function EVENT:Begin()
     self:AddHook("TTTCanSearchCorpse", function(ply, rag, covert, long_range, was_traitor)
+        -- If this player can loot credits and there are credits on the corpse, let the player loot them
+        if ply.CanLootCredits and ply:CanLootCredits() then
+            local credits = CORPSE.GetCredits(rag, 0)
+            if credits > 0 then return end
+        end
+
         if not ply:IsSpec() and ply:Alive() then
             ply:ChatPrint("This body refuses to tell you anything -- dead men tell no tales.")
         end
