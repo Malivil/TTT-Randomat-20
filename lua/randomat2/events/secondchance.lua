a039c336ed633a28f64a142206c679a2be5a88c6
Malivil <mlebson@outlook.com>
2022-05-14 17:30:28 -0400

    Changed Second Chance to hide the role of the person who killed the respawning player, if possible

--- lua/randomat2/events/secondchance.lua
+++ lua/randomat2/events/secondchance.lua
@@ -7,6 +7,7 @@ EVENT.Type = EVENT_TYPE_RESPAWN
 EVENT.Categories = {"deathtrigger", "rolechange", "moderateimpact"}
 
 function EVENT:Begin()
+    local first = nil
     self:AddHook("PlayerDeath", function(victim, entity, killer)
         if not IsValid(victim) then return end
         if not IsValid(killer) then return end
@@ -14,6 +15,7 @@ function EVENT:Begin()
         if victim == killer then return end
         self:RemoveHook("PlayerDeath")
 
+        first = victim
         victim:PrintMessage(HUD_PRINTTALK, "You will be respawning shortly...")
         victim:PrintMessage(HUD_PRINTCENTER, "You will be respawning shortly...")
         timer.Create("RdmtSecondChanceTimer", 1, 1, function()
@@ -26,6 +28,18 @@ function EVENT:Begin()
             end
             SendFullStateUpdate()
         end)
+    end)
+
+    -- Don't show the attacker's role for the player being respawned
+    self:AddHook("TTTDeathNotifyOverride", function(victim, inflictor, attacker, reason, killerName, role)
+        if not IsPlayer(first) then return end
+        if not IsPlayer(victim) then return end
+        if reason ~= "ply" then return end
+        if victim ~= first then return end
+
+        -- Reset this so they see the role the next time they die
+        first = nil
+        return reason, killerName, ROLE_NONE
     end)
 end
 
