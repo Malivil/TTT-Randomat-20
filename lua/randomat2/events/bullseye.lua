10e6efb357416a8db7d794df11ad48217f943a54
Malivil <mlebson@outlook.com>
2022-08-06 22:33:30 -0400

    Fixed Bullseye not doing damage to models that don't have head hitgroups

--- lua/randomat2/events/bullseye.lua
+++ lua/randomat2/events/bullseye.lua
@@ -6,16 +6,31 @@ EVENT.id = "bullseye"
 EVENT.Type = EVENT_TYPE_WEAPON_OVERRIDE
 EVENT.Categories = {"biased_innocent", "biased", "largeimpact"}
 
+local function WasHeadHit(ply, hitgroup)
+    if hitgroup == HITGROUP_HEAD then return true end
+
+    -- If the player wasn't hit in the head, search all the hitboxes to see if any of them belong to the head hitgroup
+    local hitbox_count = ply:GetHitBoxCount(0)
+    for i = 0, hitbox_count - 1 do
+        local box = ply:GetHitBoxHitGroup(i, 0)
+        -- If they have a head hitgroup then we know for sure they weren't hit in the head
+        if box == HITGROUP_HEAD then
+            return false
+        end
+    end
+
+    -- If they don't have a head hitgroup then any shot will count so they still take damage
+    return true
+end
+
 function EVENT:Begin()
     self:AddHook("ScalePlayerDamage", function(ply, hitgroup, dmginfo)
         -- Let the barnacle still do the damage or else people can just get stuck in there
         if IsValid(dmginfo:GetInflictor()) and dmginfo:GetInflictor():GetClass() == "npc_barnacle" then return end
+        -- If they hit the head then let it do normal damage
+        if WasHeadHit(ply, hitgroup) then return end
 
-        -- If the player doesn't have a head hitbox then they take damage as normal
-        local box = ply:GetHitBoxBone(HITGROUP_HEAD, 0)
-        if box and hitgroup ~= HITGROUP_HEAD then
-            dmginfo:ScaleDamage(0)
-        end
+        dmginfo:ScaleDamage(0)
     end)
 end
 
