a61403507004734663493dcc3bd40718cfb7a5e2
Malivil <mlebson@outlook.com>
2022-11-09 10:10:08 -0500

    Fixed traitors not being able to share the win with jesters in Fiends for Life

--- lua/randomat2/cl_events/cl_killjester.lua
+++ lua/randomat2/cl_events/cl_killjester.lua
@@ -1,5 +1,8 @@
 net.Receive("RdmtKillJesterBegin", function()
-    hook.Add("TTTScoringWinTitle", "RdmtKillJesterWinScoring", function(wintype, wintitle, title)
+    hook.Add("TTTScoringSecondaryWins", "RdmtKillJesterWinScoringSecondaryWins", function(wintype, secondary_wins)
+        -- Jester can only share win with the traitors
+        if wintype ~= WIN_TRAITOR then return end
+
         local jester_alive = false
         for _, p in ipairs(player.GetAll()) do
             if p:Alive() and not p:IsSpec() and p:GetRole() == ROLE_JESTER then
@@ -8,12 +11,13 @@ net.Receive("RdmtKillJesterBegin", function()
             end
         end
 
+        -- If the jester is alive then they share the win with the traitors
         if jester_alive then
-            return title, ROLE_JESTER
+            table.insert(secondary_wins, ROLE_JESTER)
         end
     end)
 end)
 
 net.Receive("RdmtKillJesterEnd", function()
-    hook.Remove("TTTScoringWinTitle", "RdmtKillJesterWinScoring")
+    hook.Remove("TTTScoringSecondaryWins", "RdmtKillJesterWinScoringSecondaryWins")
 end)
