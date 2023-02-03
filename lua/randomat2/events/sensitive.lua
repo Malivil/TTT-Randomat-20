37b3278fef7ab5665e8280910b6c65c8930e58da
Malivil <mlebson@outlook.com>
2023-02-02 22:16:57 -0500

    Minor cleanup

--- lua/randomat2/events/sensitive.lua
+++ lua/randomat2/events/sensitive.lua
@@ -1,5 +1,7 @@
 local EVENT = {}
 
+util.AddNetworkString("RdmtSetSensitivityValue")
+
 EVENT.Title = "Don't be so Sensitive"
 EVENT.Description = "Periodically changes each player's mouse sensitivity"
 EVENT.id = "sensitive"
@@ -9,8 +11,6 @@ CreateConVar("randomat_sensitive_change_interval", 15, {FCVAR_ARCHIVE, FCVAR_NOT
 CreateConVar("randomat_sensitive_scale_min", 25, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "The minimum sensitivity to use", 10, 100)
 CreateConVar("randomat_sensitive_scale_max", 500, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "The maximum sensitivity to use", 100, 1000)
 
-util.AddNetworkString("RdmtSetSensitivityValue")
-
 local function SetSensitivity(ply, sensitivity)
     net.Start("RdmtSetSensitivityValue")
     net.WriteFloat(sensitivity)
@@ -19,10 +19,9 @@ end
 
 function EVENT:Begin()
     local interval = GetConVar("randomat_sensitive_change_interval"):GetInt()
-
+    local min = GetConVar("randomat_sensitive_scale_min"):GetInt()
+    local max = GetConVar("randomat_sensitive_scale_max"):GetInt()
     timer.Create("RdmtSensitivityChangeTimer", interval, 0, function()
-        local min = GetConVar("randomat_sensitive_scale_min"):GetInt()
-        local max = GetConVar("randomat_sensitive_scale_max"):GetInt()
         for _, v in ipairs(player.GetAll()) do
             local sensitivity = 0
             if v:Alive() and not v:IsSpec() then
