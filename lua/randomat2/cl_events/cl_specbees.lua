1ed3f5dd38f16bffd0a58848b1502e678d77248a
Malivil <mlebson@outlook.com>
2023-02-04 17:56:16 -0500

    Fixed being able to get stuck spectating players during Rise From Your bees

--- lua/randomat2/cl_events/cl_specbees.lua
+++ lua/randomat2/cl_events/cl_specbees.lua
@@ -1,4 +1,10 @@
 net.Receive("RdmtSpecBeesBegin", function()
+    hook.Add("PlayerBindPress", "RdmtSpecBeesPlayerBindPress", function(ply, bind, pressed)
+        if not IsValid(ply) then return end
+        -- Override these buttons so the bees can mess themselves up
+        if (bind == "+use" or bind == "+duck") and pressed and ply:IsSpec() then return true end
+    end)
+
     hook.Add("Think", "RdmtSpecBeesThink", function()
         for _, e in ipairs(ents.GetAll()) do
             if e:GetNWBool("RdmtSpecBee", false) then
@@ -9,5 +15,6 @@ net.Receive("RdmtSpecBeesBegin", function()
 end)
 
 net.Receive("RdmtSpecBeesEnd", function()
+    hook.Remove("PlayerBindPress", "RdmtSpecBeesPlayerBindPress")
     hook.Remove("Think", "RdmtSpecBeesThink")
 end)
