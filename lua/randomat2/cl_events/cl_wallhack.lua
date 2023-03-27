55d2fab48ada5b61ff10d78b219a5c78d5cb7849
Malivil <mlebson@outlook.com>
2023-03-27 16:34:09 -0400

    Cleanup

--- lua/randomat2/cl_events/cl_wallhack.lua
+++ lua/randomat2/cl_events/cl_wallhack.lua
@@ -1,21 +1,27 @@
+local halo = halo
+local hook = hook
+local net = net
+
+local GetAllPlayers = player.GetAll
+
 local function IsTargetHighlighted(ply, target)
     return ply.IsTargetHighlighted and ply:IsTargetHighlighted(target)
 end
 
-net.Receive("haloeventtrigger", function()
+net.Receive("RdmtWallhackStart", function()
     local client = LocalPlayer()
-    hook.Add("PreDrawHalos", "RandomatHalos", function()
+    hook.Add("PreDrawHalos", "RdmtWallhackHalos", function()
         local alivePlys = {}
-        for k, v in ipairs(player.GetAll()) do
+        for k, v in ipairs(GetAllPlayers()) do
             if v:Alive() and not v:IsSpec() and not IsTargetHighlighted(client, v) then
                 alivePlys[k] = v
             end
         end
 
-        halo.Add(alivePlys, Color(0,255,0), 0, 0, 1, true, true)
+        halo.Add(alivePlys, Color(0, 255, 0), 0, 0, 1, true, true)
     end)
 end)
 
-net.Receive("haloeventend", function()
-    hook.Remove("PreDrawHalos", "RandomatHalos")
+net.Receive("RdmtWallhackEnd", function()
+    hook.Remove("PreDrawHalos", "RdmtWallhackHalos")
 end)
--- lua/randomat2/events/wallhack.lua
+++ lua/randomat2/events/wallhack.lua
@@ -1,15 +1,15 @@
 local EVENT = {}
 
+util.AddNetworkString("RdmtWallhackStart")
+util.AddNetworkString("RdmtWallhackEnd")
+
 EVENT.Title = "No one can hide from my sight"
 EVENT.Description = "Puts a green outline around every player"
 EVENT.id = "wallhack"
 EVENT.Categories = {"biased_innocent", "biased", "largeimpact"}
 
-util.AddNetworkString("haloeventtrigger")
-util.AddNetworkString("haloeventend")
-
 function EVENT:Begin()
-    net.Start("haloeventtrigger")
+    net.Start("RdmtWallhackStart")
     net.Broadcast()
 
     -- Bypass culling so you can always see everyone through walls
@@ -17,7 +17,7 @@ function EVENT:Begin()
 end
 
 function EVENT:End()
-    net.Start("haloeventend")
+    net.Start("RdmtWallhackEnd")
     net.Broadcast()
 end
 
