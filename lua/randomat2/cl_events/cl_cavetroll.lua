6a8fbb750a44230378b3a9716b25b0a21c270239
Malivil <mlebson@outlook.com>
2024-01-07 13:17:42 -0500

    Fixed Cave Troll infinite sprint hook name on the client conflicting with Olympic Sprint

--- lua/randomat2/cl_events/cl_cavetroll.lua
+++ lua/randomat2/cl_events/cl_cavetroll.lua
@@ -1,10 +1,10 @@
 net.Receive("RdmtCaveTrollBegin", function()
-    hook.Add("TTTSprintStaminaPost", "RdmtOlympicSprintPost", function()
+    hook.Add("TTTSprintStaminaPost", "RdmtCaveTrollSprintPost", function()
         -- Infinite sprint through fixed infinite stamina
         return 100
     end)
 end)
 
 net.Receive("RdmtCaveTrollEnd", function()
-    hook.Remove("TTTSprintStaminaPost", "RdmtOlympicSprintPost")
+    hook.Remove("TTTSprintStaminaPost", "RdmtCaveTrollSprintPost")
 end)
