7a7d83f2eb0dc1e20b95fef4debe9aa0fa68edf4
Malivil <mlebson@outlook.com>
2024-08-14 22:03:27 -0400

    Added support for TTT PropHuntGun to the Prop Hunt event's prop damage logic

--- lua/randomat2/events/prophunt.lua
+++ lua/randomat2/events/prophunt.lua
@@ -279,7 +279,8 @@ function EVENT:Begin()
 
         -- If the thing they attacked is not a disguise and not the resulting damage of exploding a barrel then they should take the damage
         -- This property is added by the Prop Disguiser [310403737] (and the Prop Disguiser Improved [2127939503])
-        if not ent.IsADisguise and not dmginfo:IsDamageType(DMG_BLAST_SURFACE) then
+        -- The NW bool is added by TTT PropHuntGun [2796353349]
+        if not ent.IsADisguise and not ent:GetNWBool("PHR_Disguised", false) and not dmginfo:IsDamageType(DMG_BLAST_SURFACE) then
             local damage_scale = GetConVar("randomat_prophunt_damage_scale"):GetFloat()
             att:TakeDamage(dmginfo:GetDamage() * damage_scale, att, att:GetActiveWeapon())
         end
