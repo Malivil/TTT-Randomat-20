075edcf2e73e64f4d98562366a67dac4d6177e61
Malivil <mlebson@outlook.com>
2024-12-04 08:42:43 -0500

    Fixed error in deadchat in TTT2 Fixed deadchat forcing ttt_limit_spectator_chat to be enabled even if it wasn't on the server before

--- lua/randomat2/events/deadchat.lua
+++ lua/randomat2/events/deadchat.lua
@@ -5,16 +5,21 @@ EVENT.Description = "Allows dead players to text chat with the living"
 EVENT.id = "deadchat"
 EVENT.Categories = {"spectator", "largeimpact"}
 
+local limitConvar = nil
 function EVENT:Begin()
-    GetConVar("ttt_limit_spectator_chat"):SetBool(false)
+    limitConvar = GetConVar("ttt_limit_spectator_chat") or GetConVar("ttt_spectators_chat_globally")
+    limitConvar:SetBool(false)
 end
 
 function EVENT:End()
-    GetConVar("ttt_limit_spectator_chat"):SetBool(true)
+    if not limitConvar then return end
+
+    limitConvar:SetBool(true)
+    limitConvar = nil
 end
 
 function EVENT:Condition()
-    return GetConVar("ttt_limit_spectator_chat"):GetBool()
+    return cvars.Bool("ttt_limit_spectator_chat", false) or cvars.Bool("ttt_spectators_chat_globally", false)
 end
 
 Randomat:register(EVENT)
